db41c2d6547dfd6d7e4e1b2a04b47f9536b9b824
Rafik Salama <rafiksays@gmail.com>
2011-06-21 11:08:45 -0400

    Starting config.js docs

--- src/config.js
+++ src/config.js
@@ -1,23 +1,51 @@
 /*global Ply, jQuery */
 /*jshint eqeqeq: true, curly: true, white: true */
 
+// The Config module provides hooks for many of the processes in Ply
+// and is meant to be modified/replaced by the user.
+
 Ply.config = (function ($) {
 
     return {
 
+        // ## Core
         core: {
+            // ### Error Logging URL
+            // The URL to log client errors to. All JavaScript errors in
+            // views are caught and logged to this URL. See `core` for more.
             errorLoggingUrl: '/error/logclienterror'
         },
 
+        // ## Read
         read: {
+            // ### URL Generator
+            // If no `__url` property is found on the view, this function is called
+            // with the view name to generate a URL. It is recommended that URLs have
+            // some computable mapping to view names in addition to DOM selectors (see below).
             urlGenerator: function (name) {
-                return '/' + name.split('_').join('/').replace('-', '').toLowerCase();
+                // #### Example
+                // Replace the underscores with slashes, remove all dashes, and prefix with
+                // a `/`, so `checkout_cart-modal`, would become `/checkout/cartmodal`
+                return '/' + name.replace('_', '/').replace('-', '').toLowerCase();
             }
         },
 
+        // ## UI
         ui: {
+            // ### Base
+            // The base object from which all views are copied. Any properties
+            // created on this object will be available to every view created
+            // using `Ply.ui.define`.
             base: {},
+            // ### Defaults
+            // The default options for each view object. When the view is started, the
+            // options defined in `defaults` are merged with the options defined on
+            // the `opts` property of the view, and any `options` passed in to `Ply.ui.register`.
             defaults: {},
+            // ### Selector generator
+            // If no `view` property is passed in to `Ply.ui.register`, the name of the view
+            // is passed to this function to generate a selector which will be passed to jQuery's
+            // selector engine.
             selectorGenerator: function (name) {
 
                 var stub = name.match(/[A-Z]?[a-z]+/g).join('_').toLowerCase(),
@@ -27,6 +55,11 @@ Ply.config = (function ($) {
 
                 return '.view-' + stub + ', .' + stub;
             },
+            // ### Register callback
+            // This function is called every time `Ply.ui.register` is called. Useful as a hook for
+            // changing the behavior of `register`. Note that if you return from this method,
+            // `Ply.ui.start` does not called, and you can short-circuit the normal process of view
+            // initialization. Use with care.
             onRegister: function (name, options) {
 
                 // Modal view
