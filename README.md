8de7dc5169f763357e5cab4f8180a9770ff989a4
Rafik Salama <rafiksalama@Rafiks-MacBook-Air.local>
2011-09-27 22:42:08 -0700

    Bumping version

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Ply
 
-**Current Version**: 0.2.2<br />
+**Current Version**: 0.2.3<br />
 **Last Updated**: September 27, 2011
--- src/core.js
+++ src/core.js
@@ -16,7 +16,7 @@
 // Declare global namespace and assign version number.
 
 window.Ply = {
-    VERSION: '0.2.2'
+    VERSION: '0.2.3'
 };
 
 // Define `core` module.
